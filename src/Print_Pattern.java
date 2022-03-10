02dde17d49871ebe8e5e3696a9d4881f941afd13
John <jonualex07895998212@gmail.com>
2022-03-10 09:54:17 +0530

    Pattern_Trial

---
+++ src/Print_Pattern.java
@@ -0,0 +1,12 @@
+
+public class Print_Pattern {
+	public static void main()
+	{
+		for(int i = 0;i<5;i++)
+		{
+		  for(int j = 0;j<i;j++)
+			System.out.print("#");
+		  System.out.println();
+		}
+	}
+}
